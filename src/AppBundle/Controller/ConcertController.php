ba0942b52187fd27262f504d532c0613a2be3057
Hiroki Hasegawa <hasegawafeedshop@gmail.com>
2019-03-30 01:30:07 +0900

    update #1 forループとTwigを用いて、公演情報の配列変数を出力

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -17,6 +17,41 @@ class ConcertController extends Controller
      */
     public function indexAction()
     {
-        return $this->render("Concert/index.html.twig");
+        $concertList = [ //連想配列を定義
+            [
+                'date' => '2015年5月3日',
+                'time' => '14:00',
+                'place' => '東京文化会館(満席)',
+                'available' => false, //予測可能フラグを立てて、表示を分岐させる
+            ],
+            [
+                'date' => '2015年7月12日',
+                'time' => '14:00',
+                'place' => '鎌倉芸術館',
+                'available' => true,
+            ],
+            [
+                'date' => '2015年9月20日',
+                'time' => '15:00',
+                'place' => '横浜みなとみらいホール',
+                'available' => true,
+            ], 
+            [
+                'date' => '2015年11月8日',
+                'time' => '15:00',
+                'place' => 'よこすか芸術劇場(満席)',
+                'available' => false,
+            ], 
+            [
+                'date' => '2016年1月10日',
+                'time' => '14:00',
+                'place' => '渋谷公会堂',
+                'available' => true,
+            ],
+        ];
+
+        return $this->render("Concert/index.html.twig",
+            ['concertList' => $concertList] //公演情報配列をテンプレートへ渡す
+        );
     }
 }
