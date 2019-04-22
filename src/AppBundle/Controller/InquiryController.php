93cd287bd1ef564cd90014633d6ba79292652a89
Hiroki Hasegawa <hasegawafeedshop@gmail.com>
2019-04-23 01:51:23 +0900

    update #6 フォームとエンティティの連携

--- src/AppBundle/Controller/InquiryController.php
+++ src/AppBundle/Controller/InquiryController.php
@@ -35,27 +35,32 @@ class InquiryController extends Controller
         $form->handleRequest($request); #formオブジェクトから呼び出す。クライアントから送信された情報をフォームオブジェクトに取り込む
         if($form->isValid()) #もし入力値が正しかった場合、データベースへ情報を反映し、通知メールを送り、完了ページへリダイレクトする。
         { 
-            $data = $form->getData();
-
+            /*『return $this->createFormBuilder(new Inquiry())』を追加したのでいらない。
+            
+            $data = $form->getData(); #フォームオブジェクトの入力データ全体を連想配列として取り出し、$dataに格納
             $inquiry = new Inquiry(); #Inquiryエンティティからインスタンスを作成
-            $inquiry->setName($data['name']);
+            $inquiry->setName($data['name']); #$inquiryのsetName()で$dataからname値を取り出す
             $inquiry->setEmail($data['email']);
             $inquiry->setTel($data['tel']);
             $inquiry->setType($data['type']);
             $inquiry->setContent($data['content']);
+            
+            */
+
+            $inquiry = $form->getData(); #フォームオブジェクトの入力データ全体を連想配列として取り出し、$inquiryに格納
 
             $em = $this->getDoctrine()->getManager(); #Entityマネージャを取得
             $em->persist($inquiry); #InquiryエンティティのインスタンスをDoctrineの管理下へ
             $em->flush(); #変更をデータベースへ反映
 
-            $message = \Swift_Message::newInstance() #メールメッセージの作成
+            $message = \Swift_Message::newInstance() #メールメッセージオブジェクトの作成
                 ->setSubject('Webサイトからのお問い合わせ') #メールの件名を設定
                 ->setFrom('webmaster@example.com')
                 ->setTo('admin@example.com')
                 ->setBody(
                     $this->renderView( #テンプレートから本文を作成
                         'mail/inquiry.txt.twig',
-                        ['data' => $data]
+                        ['data' => $inquiry] #$dataをキーに、$inquiryをバリューとする。
                     )
                 );
             
@@ -65,8 +70,8 @@ class InquiryController extends Controller
                 $this->generateUrl('app_inquiry_complete')); #何らかの処理を行った後、指定の『ルート名』にリダイレクト（php bin/console debug:routerで確認）
         } 
         
-        return $this->render('Inquiry/index.html.twig', #同じclass内のメンバ変数を使うために疑似変数を使用。
-            ['form' => $form->createView()] #入力エラーの場合は同じフォームを出力
+        return $this->render('Inquiry/index.html.twig', #同じclass内のメンバ変数を使うために疑似変数を使用。#入力エラーの場合は同じフォームを出力
+            ['form' => $form->createView()] #$formをキーに、createViewの返り値をバリューとする。
         );
     }
 
@@ -82,7 +87,7 @@ class InquiryController extends Controller
 
     private function createInquiryForm() #フォームを定義する関数を作成
     {
-        return $this->createFormBuilder() #同じclass内のメンバ変数を使うために疑似変数を使用。
+        return $this->createFormBuilder(new Inquiry()) #同じclass内のメンバ変数を使うために疑似変数を使用。#フォームのデータをEntityのインスタンスに格納。
             ->add('name', TextType::class) #add()メソッドを呼び出し、#第１引数：フィールドの識別名、第２引数：フィールドのタイプ、第３引数：フィールドのオプションを連想配列で指定
             ->add('email', EmailType::class) 
             ->add('tel', TelType::class, [
