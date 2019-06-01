db5e2cd1075ac046c0bf657a8e93ee27c65c1c0c
Hiroki Hasegawa <hasegawafeedshop@gmail.com>
2019-06-01 12:45:47 +0900

    update #10 APIエンドポイント用クラスに公演情報を返すアクションを追加

--- src/AppBundle/Controller/Api/ConcertController.php
+++ src/AppBundle/Controller/Api/ConcertController.php
@@ -3,9 +3,30 @@
 
 namespace AppBundle\Controller\Api;
 
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 
-class ConcertController extends FOSRestController
+class ConcertController extends AbstractFOSRestController
 {
+    /**
+     * #アノテーションのパラメータに拡張子を用いる
+     * @Rest\Get("/api/concerts.{_format}")
+     */
+    public function getConcertsAction()
+    {
+        $em = $this->get('doctrine')->getManager();
 
+        #対象とするEntityを引数で指定
+        $repository = $em->getRepository('AppBundle:Concert');
+
+        #findAllでデータを取得
+        $concertList = $repository->findAll();
+
+        #取得したデータからViewオブジェクトを作成
+        $view = new View($concertList);
+
+        #Viewオブジェクトからレスポンスオブジェクトを作成
+        return $this->handleView($view);
+    }
 }
