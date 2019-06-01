20f2865c6c9a354752835008b4d689a9bfbbe198
Hiroki Hasegawa <hasegawafeedshop@gmail.com>
2019-06-01 15:10:26 +0900

    update #10 handleView()が自動的に実行されるように修正

--- src/AppBundle/Controller/Api/ConcertController.php
+++ src/AppBundle/Controller/Api/ConcertController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller\Api;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
 
 class ConcertController extends AbstractFOSRestController
 {
@@ -23,10 +22,7 @@ class ConcertController extends AbstractFOSRestController
         #findAllでデータを取得
         $concertList = $repository->findAll();
 
-        #取得したデータからViewオブジェクトを作成
-        $view = new View($concertList);
-
-        #Viewオブジェクトからレスポンスオブジェクトを作成
-        return $this->handleView($view);
+        #エンティティの配列をそのままreturn
+        return $concertList;
     }
 }
