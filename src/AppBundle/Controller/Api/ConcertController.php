301a13f3d461f70933b454c9614019e939e996d3
Hiroki Hasegawa <hasegawafeedshop@gmail.com>
2019-06-01 15:47:26 +0900

    update #10 ルート定義をアクションメソッド名から自動的に決定できるように修正

--- src/AppBundle/Controller/Api/ConcertController.php
+++ src/AppBundle/Controller/Api/ConcertController.php
@@ -4,14 +4,9 @@
 namespace AppBundle\Controller\Api;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 
 class ConcertController extends AbstractFOSRestController
 {
-    /**
-     * #アノテーションのパラメータに拡張子を用いる
-     * @Rest\Get("/api/concerts.{_format}")
-     */
     public function getConcertsAction()
     {
         $em = $this->get('doctrine')->getManager();
